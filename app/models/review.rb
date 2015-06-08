81971b6dccb7bc302bced9dbb4a7d2963dd24a55
gerardodelapiedra <gerardo.delapiedra@gmail.com>
2015-06-08 16:52:22 -0500

    Changes_Validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,9 +2,17 @@ class Review < ActiveRecord::Base
 
   validates :user_id, :presence => true
   validates :venue_id, :presence => true
+  validates :general_rating, :presence => true, :numericality => { :greater_than => 0, :less_than => 6, :only_integer => true }
+  validates :dish_name, :presence => true
+  validates :dish_rating, :presence => true
+  validates :atmosphere_rating, :presence => true
+  validates :average_per_person_price, :presence => true
+
+
 
   belongs_to :user
   belongs_to :venue
+
 
 
 
--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,6 +1,8 @@
 class Venue < ActiveRecord::Base
 
   validates :name, :presence => true, :uniqueness => { :scope => :address }
+  validates :address, :presence => true
+  validates :main_cuisine, :presence => true
 
 
   has_many :reviews
