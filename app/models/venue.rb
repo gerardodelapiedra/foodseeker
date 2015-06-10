fd74a36bec5e75fced378f416e53adaed50c6e4c
gerardodelapiedra <gerardo.delapiedra@gmail.com>
2015-06-09 19:02:13 -0500

    Changes_Final Version
    Corrected Venue validations

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,9 +1,9 @@
 class Venue < ActiveRecord::Base
 
   validates :name, :presence => true, :uniqueness => { :scope => :address }
-  validates :name, :uniqueness => true
+  #validates :name, :uniqueness => true
   validates :address, :presence => true, :uniqueness => true
-  validates :main_cuisine, :presence => true, :uniqueness => true
+  validates :main_cuisine, :presence => true
 
 
 
